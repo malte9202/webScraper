4f3da2c2173657137eee748f7cc8fd342e0d9914
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-08-23 21:44:58 +0200

    email function and split of functionality

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 url = 'https://geizhals.de/samsung-c27f398-lc27f398fwuxen-a1490511.html'
 
@@ -8,17 +9,43 @@ headers = {
                   '(KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'
 }
 
+reference_price = 150.00
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-def check_price():
-    page = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.select_one(".variant__header__headline").get_text(strip=True)
+def get_title() -> str:
+    title = soup.select_one('.variant__header__headline').get_text(strip=True)
+    return title
+
+
+def get_price() -> float:
     price = float(soup.select_one(".gh_price").get_text(strip=True)[2:].replace(",", "."))
-    print(title)
-    print(price)
+    return price
 
 
-check_price()
+def check_price(price: float):
+    if price < reference_price:
+        send_mail()
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('email', 'password')
+    subject = 'Price fell down!'
+    body = 'Check the Geizhals link: https://geizhals.de/samsung-c27f398-lc27f398fwuxen-a1490511.html'
+    message = f'Subject: {subject}\n\n{body}'
+    server.sendmail(
+        'sender',
+        'empfänger',
+    )
+    print('Email has been sent.')
+    server.quit()
+
+
+check_price(149)
 
 
